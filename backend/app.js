5d1d6f1c6dc8f852230c863f6de344544bd10d43
miguel2433 <miguelitocap12@gmail.com>
2025-11-07 03:50:57 -0300

    disable cors and add script dev

--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(helmet());
 app.use(cors({
-    origin: '*',
+    origin: 'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
