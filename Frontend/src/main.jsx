2742b1a2421d61cdb343cdb235d079b4436d4627
miguel2433 <miguelitocap12@gmail.com>
2025-11-06 23:04:17 -0300

    feat: descarga de dependencias

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,13 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { Page } from './pages/Page'
-import Dashboard from './pages/Dashboard'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <
+    
     {/* <Page /> */}
   </StrictMode>,
 )
